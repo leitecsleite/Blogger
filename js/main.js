1a0571cca70849fc2ab9daf31e527eb998967eed
Lucas Leite <70826073+leitecsleite@users.noreply.github.com>
2020-10-08 16:55:05 -0300

    enter

--- js/main.js
+++ js/main.js
@@ -7,6 +7,17 @@ $(document).ready(function(){
    $toggleCollapse.click(function(){
        $nav.toggleClass('collapse');
    })
+   
+   //owl-corousel for blog 
 
+   $('.owl-carousel').owlCarousel({
+      loop:true, 
+      autoplay:true,  
+      autoplayTimeout:3000,  
+      dots:false,
+      nav:true,
+      navText:[$('.owl-navegation .owl-nav-prev'), $('.owl-navegation .owl-nav-next')]
+
+   }); 
 
 })
