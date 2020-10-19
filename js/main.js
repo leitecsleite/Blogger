7a7ebd7a06a6d87c2d2ceb99c3d877f6f49d4a2c
Lucas Leite <70826073+leitecsleite@users.noreply.github.com>
2020-10-19 16:43:13 -0300

    Projeto Finalizado

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+
+const responsive = {
+   0 : {
+     items: 1
+   },
+   
+   320:{
+      items: 1
+   }, 
+   560:{
+    items: 2
+   },
+
+   960: {
+      items:3
+   }
+}
+
+
 $(document).ready(function(){
    $nav = $('.nav');
    $toggleCollapse =$('.toggle-collapse');
@@ -16,8 +35,18 @@ $(document).ready(function(){
       autoplayTimeout:3000,  
       dots:false,
       nav:true,
-      navText:[$('.owl-navegation .owl-nav-prev'), $('.owl-navegation .owl-nav-next')]
-
+      navText:[$('.owl-navegation .owl-nav-prev'), $('.owl-navegation .owl-nav-next')],
+      responsive:responsive
    }); 
+  
+   // click to scrooll top 
 
-})
+   $(".move-up span").click(function(){
+      $('html, body').animate({
+         scrollTop: 0
+      }, 1000); 
+   })
+
+   // Aos instance
+    AOS.init();
+});
